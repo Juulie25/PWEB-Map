4e464b583c1cae09855620beb03e07e9a96f16ae
TBrioche <72711057+TBrioche@users.noreply.github.com>
2022-02-24 12:05:41 +0100

    map sans nom de pays

--- front/js/accueil.js
+++ front/js/accueil.js
@@ -5,7 +5,7 @@ let randomElement;
 
 function init(){
     map = L.map('mapDiv').setView([42.607752 , -13.542906],2);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png').addTo(map);
     map.doubleClickZoom.disable();
 
     let doc =  document.getElementById("nvlPartie");
