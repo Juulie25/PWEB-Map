74dcd86709cf5ad1c52405e612066ff27271d78f
julie pessey <julie.pessey@etu.u-paris.fr>
2022-02-25 07:58:30 +0100

    trigger finished
    fin de mise en place du trigger
    pas d'erreur lors de l'execution mais l'insertion ne se fait pas encore dans la table stats

--- bdd/scriptConnect.sql
+++ bdd/scriptConnect.sql
@@ -33,42 +33,29 @@ ALTER TABLE stats
 ADD CONSTRAINT FK_Stats FOREIGN KEY (IdJoueur) REFERENCES joueur(IdJoueur); 
 
 
-INSERT INTO joueur (nomJoueur, MotDePasse) VALUES ("Thomas", "monMotDePasse0");
-INSERT INTO joueur (nomJoueur, MotDePasse) VALUES ("Axel", "123456789ABC");
-INSERT INTO joueur (nomJoueur, MotDePasse) VALUES ("Julie", "/password12");
+INSERT INTO joueur(nomJoueur, MotDePasse) VALUES ("Thomas", "monMotDePasse0");
+INSERT INTO joueur(nomJoueur, MotDePasse) VALUES ("Axel", "123456789ABC");
+INSERT INTO joueur(nomJoueur, MotDePasse) VALUES ("Julie", "/password12");
 
-INSERT INTO partie (IdJoueur, score) VALUES (1, 19); 
-INSERT INTO partie (IdJoueur, score) VALUES (2, 16); 
-INSERT INTO partie (IdJoueur, score) VALUES (3, 22); 
-INSERT INTO partie (IdJoueur, score) VALUES (2, 25); 
-INSERT INTO partie (IdJoueur, score) VALUES (1, 20); 
+INSERT INTO partie(IdJoueur, score) VALUES (1, 19); 
+INSERT INTO partie(IdJoueur, score) VALUES (2, 16); 
+INSERT INTO partie(IdJoueur, score) VALUES (3, 22); 
+INSERT INTO partie(IdJoueur, score) VALUES (2, 25); 
+INSERT INTO partie(IdJoueur, score) VALUES (1, 20); 
 
 DELIMITER $$
 CREATE OR REPLACE TRIGGER T_AjoutStats
 BEFORE INSERT ON partie
 FOR EACH ROW 
 BEGIN
-DECLARE done INT DEFAULT 0;
 DECLARE joueurID int;
 DECLARE joueurScore int; 
 DECLARE joueurNnParties int; 
-DECLARE cJoueur CURSOR FOR SELECT IdJoueur FROM joueur;
-DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-
-OPEN cJoueur;
-label: LOOP
-FETCH cJoueur INTO joueurID;
-IF cJoueur == NEW.IdJoueur THEN 
-        joueurID = tupleStats.IdJoueur;
-END IF; 
-IF done = 1 THEN LEAVE label;
-END IF;
-END LOOP;
-CLOSE cJoueur;
-
-SELECT p.IdJoueur, p.score, COUNT(s.nbParties) INTO joueurID, joueurScore , joueurNnParties FROM partie p, stats s WHERE p.IdJoueur = s.IdJoueur
+SELECT p.score, COUNT(s.nbParties) INTO joueurScore , joueurNnParties FROM partie p, stats s WHERE p.IdJoueur = s.IdJoueur AND p.IdJoueur = NEW.IdJoueur;
 IF joueurScore > NEW.score THEN 
-    INSERT INTO stats(IdJoueur, meilleurScore, nbParties) VALUES (joueurID, joueurScore , joueurNnParties +1);
+    INSERT INTO stats(IdJoueur, meilleurScore, nbParties) VALUES (NEW.IdJoueur, joueurScore , joueurNnParties +1);
 END IF; 
 END; 
 $$
+
+INSERT INTO partie(IdJoueur, score) VALUES (1, 30); 
