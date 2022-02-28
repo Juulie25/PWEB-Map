99206e072ffca905d75594b875625a7d8ec44be3
julie pessey <julie.pessey@etu.u-paris.fr>
2022-02-28 15:06:13 +0100

    maj de la structure

---
+++ bdd/scriptConnect.sql
@@ -0,0 +1,61 @@
+DROP TABLE IF EXISTS joueur; 
+DROP TABLE IF EXISTS partie;
+DROP TABLE IF EXISTS stats; 
+
+
+CREATE TABLE joueur (
+    IdJoueur int NOT NULL AUTO_INCREMENT,
+    nomJoueur varchar(50), 
+    MotDePasse varchar(50), 
+    PRIMARY KEY (IdJoueur)
+);
+
+CREATE TABLE partie (
+    IdPartie int NOT NULL AUTO_INCREMENT,
+    IdJoueur int, 
+    score int, 
+    PRIMARY KEY (IdPartie)
+);
+
+CREATE TABLE stats (
+    IdJoueur int, 
+    meilleurScore int, 
+    nbParties int
+);
+
+ALTER TABLE partie 
+ADD CONSTRAINT FK_Partie FOREIGN KEY(IdJoueur) REFERENCES joueur(IdJoueur); 
+
+ALTER TABLE partie
+ADD CONSTRAINT CK_Score CHECK (score > 0); 
+
+ALTER TABLE stats
+ADD CONSTRAINT FK_Stats FOREIGN KEY (IdJoueur) REFERENCES joueur(IdJoueur); 
+
+
+INSERT INTO joueur(nomJoueur, MotDePasse) VALUES ("Thomas", "monMotDePasse0");
+INSERT INTO joueur(nomJoueur, MotDePasse) VALUES ("Axel", "123456789ABC");
+INSERT INTO joueur(nomJoueur, MotDePasse) VALUES ("Julie", "/password12");
+
+INSERT INTO partie(IdJoueur, score) VALUES (1, 19); 
+INSERT INTO partie(IdJoueur, score) VALUES (2, 16); 
+INSERT INTO partie(IdJoueur, score) VALUES (3, 22); 
+INSERT INTO partie(IdJoueur, score) VALUES (2, 25); 
+INSERT INTO partie(IdJoueur, score) VALUES (1, 20); 
+
+DELIMITER $$
+CREATE OR REPLACE TRIGGER T_AjoutStats
+BEFORE INSERT ON partie
+FOR EACH ROW 
+BEGIN
+DECLARE joueurID int;
+DECLARE joueurScore int; 
+DECLARE joueurNnParties int; 
+SELECT p.score, COUNT(s.nbParties) INTO joueurScore , joueurNnParties FROM partie p, stats s WHERE p.IdJoueur = s.IdJoueur AND p.IdJoueur = NEW.IdJoueur;
+IF joueurScore > NEW.score THEN 
+    INSERT INTO stats(IdJoueur, meilleurScore, nbParties) VALUES (NEW.IdJoueur, joueurScore , joueurNnParties +1);
+END IF; 
+END; 
+$$
+
+INSERT INTO partie(IdJoueur, score) VALUES (1, 30); 
---
+++ front/inscription.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Inscription</title>
+</head>
+<body>
+<form action="" method="">
+  <label for="pseudo">Pseudo</label><br>
+  <input type="text" id="pseudo" name="pseudo"><br>
+  <label for="password">Mot de passe</label><br>
+  <input type="text" id="password" name="password">
+</form>
+
+
+</body>
+</html>
