fb724c285f647915f67d1e5ddc92d39dc55c88cf
julie pessey <julie.pessey@etu.u-paris.fr>
2022-03-03 15:18:50 +0100

    add php files
    ajout de fichiers php permettant la connexion à la base de données

---
+++ model/utilisateursBD.php
@@ -0,0 +1,70 @@
+<?php
+
+function getJoueur($nom, $motdepasse, &$attributs = array())
+{
+    require_once "./model/connectBD.php";
+    $pdo = PDO();
+
+    $sql = "SELECT * FROM joueur WHERE nomJoueur = :nom AND MotDePasse = :motdepasse";
+
+    try {
+        $cmd = $pdo->prepare($sql);
+        $cmd->bindParam(":nomJoueur", $nom);
+        $cmd->bindParam(":MotDePasse", $motdepasse);
+
+        $exec = $cmd->execute();
+
+        if ($exec) {
+            $attributs = $cmd->fetchAll(PDO::FETCH_ASSOC);
+            return $attributs;
+        }
+        return false;
+    } catch (PDOException $e) {
+        echo utf8_encode("Erreur lors de la récupération : " . $e->getMessage() . "\n");
+        die();
+    }
+}
+
+function ajouterJoueur($nomJoueur, $MotDePasse)
+{
+    require_once "./model/connectBD.php";
+    $pdo = PDO();
+
+    $sql = "INSERT INTO joueur VALUES (:nomJoueur, :MotDePasse)";
+
+    try {
+        $cmd = $pdo->prepare($sql);
+        $cmd->bindParam(":nomJoueur", $nomJoueur);
+        $cmd->bindParam(":MotDePasse", $MotDePasse);
+
+        return $cmd->execute();
+
+    } catch (PDOException $e) {
+        echo utf8_encode("Erreur lors de l'insertion : " . $e->getMessage() . "\n");
+        die();
+    }
+}
+
+
+function getJoueursSats(&$joueurs){
+
+    require_once "./model/connectBD.php";
+    $pdo = PDO();
+
+    $sql = "SELECT * FROM stats";
+
+    try {
+        $cmd = $pdo->prepare($sql);
+        $exec = $cmd->execute();
+
+        if ($exec) {
+            $joueurs = $cmd->fetchAll(PDO::FETCH_ASSOC);
+            return $joueurs; 
+        }
+        return false;
+
+    } catch (PDOException $e) {
+        echo utf8_encode("Erreur lors de la récupération : " . $e->getMessage() . "\n");
+        die();
+    }
+}
